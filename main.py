9f70f3f1bc21a0a6fe710811f173afea05331213
unknown <jeevanvaishnavv@gmail.com>
2022-01-28 22:25:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+
+
+Builder.load_file('main.kv')
+
+
+class MyLayout(Widget):
+    def switch_click(self, switchObj, switchValue):
+        if(switchValue):
+            self.ids.my_label.text = "You Click the Switch On!!"
+        else:
+            self.ids.my_label.text = "You Click the Switch Off!!"
+            self.ids.my_switch.disabled = True
+
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == "__main__":
+    AwesomeApp().run()
